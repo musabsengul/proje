759fb95e942867fdd6b1a49b98667c8762498ee6
Musab Şengül <66556529+musabsengul@users.noreply.github.com>
2020-06-26 22:35:54 +0300

    Added new files

---
+++ precache-manifest.c27770080bfedb44d71fd516346cf342.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bb7af4df3b5076cae103ee8c018f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "010128ff665815aecc03",
+    "url": "/static/css/2.554ab22e.chunk.css"
+  },
+  {
+    "revision": "61a3f07e63af2d95813a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "010128ff665815aecc03",
+    "url": "/static/js/2.2d6610b2.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.2d6610b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a3f07e63af2d95813a",
+    "url": "/static/js/main.35c30904.chunk.js"
+  },
+  {
+    "revision": "955d07b3112ffa10e906",
+    "url": "/static/js/runtime-main.bb5b2b8f.js"
+  }
+]);
